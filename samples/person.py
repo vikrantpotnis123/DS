044287dbd373bcc33a7bf5d02817a2f17ab98ae3
Vikrant Potnis <vikrant.potnis@gmail.com>
2020-05-23 18:38:20 -0700

    tweaks to person.py

--- samples/person.py
+++ samples/person.py
@@ -2,13 +2,22 @@
 class Person:
     color = 'blue'
 
-    def __init__(self, color):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
 
     def __init__(self, name, lname):
+        print('init')
         self.name = name
         self.lname = lname
+
+    def __enter__(self):
+        # print("hello", self.name, self.lname)
+        print("enter")
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print("exit")
 
     def hello(self):
         print("hello", self.name, self.lname)
@@ -17,10 +26,17 @@ class Person:
         print("hi", self.name, self.color)
 
 def main():
+    '''
     p = Person("V", "P")
     p.hello()
     p.hi()
+    '''
+
     import keyword
     print(keyword.kwlist)
+
+    with Person('p', 'p') as p1:
+        p1.hello()
+
 
 main()
